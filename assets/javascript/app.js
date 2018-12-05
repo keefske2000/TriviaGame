765646725894163294efac201f57cd9397ab8461
keefske2000 <keith.w.collins@gmail.com>
2018-12-04 22:50:13 -0500

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,239 +1,69 @@
 
 
 var questions = "Entertainment: Television";
- var queryURL = "https://opentdb.com/api.php?amount=10&category=14&difficulty=easy&type=multiple";
+var queryURL = "https://opentdb.com/api.php?amount=10&category=14&difficulty=easy&type=multiple";
+var currentQuestion = 0;
+var correct = 0;
+var incorrect = 0;
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
+//Get questions from the api
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function (response) {
 
-        var question1 = $("#question");
-        var answers1 = $("#answer");
-        var incorrect1 = $("#incorrect1");
-        var incorrect2 = $("#incorrect2");
-        var incorrect3 = $("#incorrect3");
+  //Show me the repsone 
+  console.log(response.response_code)
 
-        console.log(queryURL)
-
-        console.log(response)
-      // Create a new table row element
-     
-
-      // Methods run on jQuery selectors return the selector they we run on
-      // This is why we can create and save a reference to a td in the same statement we update its text
-     var firstQ = $("#question").text("Question:" + response.question1);
-      var firstA = $("#answer").text("Choices:" + response.answers1);
-      var firstWrong = $("#incorrect1").text(response.incorrect1);
-      var secondWrong = $("#incorrect2").text(respones.incorrect2);
-      var thridWrong = $("#incorrect3").text(response.incorrect3);
-      
-        
-      // Append the newly created table data to the table row
-      question1.append(firstQ);
-      answers1.append(firstA);
-      incorrect1.append(firstWrong);
-      incorrect2.append(secondWrong);
-      incorrect3.append(thirdWrong);
-
-    //   // Append the table row to the table body
-      $("#question").append(question1);
-      $("#answer").append(answers1);
-      $("#incorrect1").append(incorrect1);
-      $("#incorrect2").append(incorrect2);
-      $("#incorrect3").append(incorrect3);
-  
-
-     
-    });
-
-
-    $(document).ready(function() 
-{    $(".form-check").click(function() {   
-
-    if ($("input[@name=exampleRadios]:checked").val() ||
-     {
-        alert("There are more !");
+  //Get array of questions
+  var questions = response.results
+  console.log(questions)
+  var ans = []
+  //Show each question and answers
+  for (var question in questions) {
+    var allAnswers = [questions[question].correct_answer]
+    for (var q in questions[question].incorrect_answers) {
+      allAnswers.push(questions[question].incorrect_answers[q])
     }
-
-    else {
-        var answers1 = "1";
-
-
-        var answers1 = ($("input[@name=exampleRadios]:checked").val() = "a");
-    
-    if (answers1) {}
-    
-    
-    
+    var quesObj = {
+      corAnswer: questions[question].correct_answer, question: questions[question].question,
+      answers: allAnswers
     }
+    ans.push(quesObj)
+  }
 
+  $("#answers").append(`<h1>${ans[currentQuestion].question}</h1>`)
+// Ineed to complete for loop identifiers to cycle through arraty questions for game
+  for (var i in ans[currentQuestion].answers) {
+    $("#answers").append(`<div class="form-check">
+      <input class="form-check-input" type="radio" name="exampleRadios" id="incorrect1" value="${ans[currentQuestion].answers}" checked>
+      <label class="form-check-label" for="exampleRadios5">
+          ${ans[currentQuestion].answers[i]}
+      </label>
+    </div>`)
+  }
 
+})
 
+// complete document.ready function for results of answers and incorrect answers 
 
+$(document).ready(function () {
+  $(".form-check").click(function () {
 
+    if ($("input[@name=exampleRadios]:checked").val() || $()) {
+      alert("There are more !");
 
+    } else {
+      var answers1 = "1";
 
 
+      var answers1 = ($("input[@name=exampleRadios]:checked").val() = "a");
 
+    }
+  })
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $("#start").on("click", function() {
-
-//     var questions = $(this).attr("#start");
-
-//     $.ajax({method:"GET", url:`https://opentdb.com/api.php?amount=${number}&category=${cat}&difficulty=${d}&type=multiple`, success:function(results){
-//                 appendToPage(results)
-//             }}).done(() => {
-//                 alert("Let the Questions Begin ")
-//             })
-        
-
-//         .then(function(results) {
-//             console.log(url);
-
-//             comnsole.log(results);
-
-//             var quiz = url.results.data;
-
-//             for (var i = 0; i < quiz.length; i++) {
-
-//                var questions = $("#question");
-
-//                var answers = $("#answer").text("Multiple Choice" + quiz[i].answers);
-
-
-//                quiz.append(questions);
-//                quiz.append(answers);
-//             }
-//         });
-//     });
-
-
-           
-             
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var questions = {
-//     ask: function(h) {
-//         console.log(`Here is the H: ${h}`)
-//     }
-// }
-
-// questions.ask("Hello from the object")
-
-// // var question1 = {
-// //         first: "For every customer complaint how many other unhappy customers do you think have not reported their own customer service complaint remaining silent ?"
-
-// // }
-
-// // var question2 = {
-// //         second: "How many people will a dissatisfied cusotmer tell about their experience ?"
-
-// // }
-
-// // var question3 = {
-// //         third: "How many positive expereinces does it takes to make up for one unresolved negative experience ?"
-
-// // }
-
-// function appendToPage(questions){
-//     console.log(`hello from append ${questions}to page`)
-
-// }
-
-// function getQuestions(questions) {
-//     var user ="Keith"
-   
-    
-//     $.ajax({method:"GET", url:`https://opentdb.com/api.php?amount=${number}&category=${cat}&difficulty=${d}&type=multiple`, success:function(results){
-//         appendToPage(results)
-//     }}).done(() => {
-//         alert("Let the Questions Begin ")
-//     })
-// }
-
-// getQuestions("easy",10,9)
-// var user = questions.ask.call(questions);
-// document.getElementById("#question").innerHTML = user;
+// Add delayButtonAlert for the start and finish of the game.
 
 // var delayButtonAlert;
 
